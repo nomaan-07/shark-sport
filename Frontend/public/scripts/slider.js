e0820fa60d06b0008c6781a964c9cf205021f10f
amirrezash0n <shourvarziamirreza@gmail.com>
2024-09-23 19:36:45 +0330

    Add: Frontend

---
+++ Frontend/public/scripts/slider.js
@@ -0,0 +1,101 @@
+const swiperStoris = new Swiper(".stories__swiper", {
+  loop: true,
+  lazy: true,
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+  },
+  freeMode: {
+    enabled: true,
+    sticky: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 3.5,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 70,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 75,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 50,
+    },
+    1400: {
+      slidesPerView: 8,
+      spaceBetween: 20,
+    },
+  },
+});
+const swiperBrands = new Swiper(".brand-swiper", {
+  loop: true,
+  lazy: true,
+  slidesPerView: 2.5,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  freeMode: {
+    enabled: true,
+    sticky: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 3.5,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 70,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 75,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 50,
+    },
+    1400: {
+      slidesPerView: 7,
+      spaceBetween: 48,
+    },
+  },
+});
+const swiperHomeProduct = new Swiper(".home-product-swiper", {
+  loop: true,
+  lazy: true,
+  slidesPerView: 1.8,
+  spaceBetween: 10,
+  // autoplay: {
+  //   delay: 2000,
+  // },
+  freeMode: {
+    enabled: true,
+    sticky: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2.5,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    // 1200: {
+    //   slidesPerView: 6,
+    // },
+    1400: {
+      slidesPerView: 4.60,
+    },
+  },
+});
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container:{
+        center: true,
+        padding:{
+          DEFAULT : "1rem", // 16px
+          lg : "4.375rem", // 70px
+        }
+      },
+      colors:{
+        blueBrand : "#4455E8",
+        orangeBrand : "#FF9447",
+        orangeTint_1 : "#FFDEC7",
+        orangeTint_3 : "#F29F64",
+        grayTint_1 : "#E6E6E6",
+        grayTint_2 : "#CCC",
+        grayTint_3 : "#B3B3B3",
+        grayTint_4 : "#999999",
+        grayShade_1 : "#666666",
+        overlayBcg : "#00000066",
+        neutralBorder : "#E0E0E2",
+      },
+      backgroundImage : {
+        "custom-gradient" : 'linear-gradient(180deg, rgba(33, 33, 33, 0.53) 53%, #212121 100%)',
+        "custom-gradient2" : 'linear-gradient(180deg, rgba(33, 33, 33, 0.53) 100%, #212121 53%)',
+        "custom-gradient3" : 'linear-gradient(90deg, rgba(243, 67, 67, 0.58) 0%, #F64F77 100%)',
+      },
+      fontFamily : {
+        "dana": "Dana Regular",
+        "danaMedium": "Dana Medium",
+        "danaDemiBold": "Dana DemiBold",
+        "morabbaMedium" : "Morabba Medium",
+        "morabbaLight" : "Morabba Light",
+        "morabbaBold" : "Morabba Bold",
+        "vazirRegular" : "Vazir Regular"
+      },
+      dropShadow : {
+        "bannerTitle" : "0 5px .6px rgba(0,0,0,.25)"
+      },
+      fontSize: {
+        '32': '2rem',
+      },
+    },
+    screens :{
+      'xs': '576px',
+      'sm': '768px',
+      'md': '992px',
+      'lg': '1200px',
+      'xl': '1400px',
+    },
+  },
+  plugins: [],
+}
