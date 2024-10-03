835907da4a009caeb7445daec46218bdd3c1ee0b
nomaan-07 <nomaan07.dev@gmail.com>
2024-10-03 11:34:09 +0330

    Feat: Panel settings page

---
+++ Frontend/public/panel/user/js/settings.js
@@ -0,0 +1,5 @@
+const imageUploadBtn = document.querySelector(".image-upload-btn");
+const imageUploadInput = document.querySelector(".image-upload-input");
+
+
+imageUploadBtn.addEventListener("click", () => imageUploadInput.click())
